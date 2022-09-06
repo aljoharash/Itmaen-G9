d948d0499da7f3bb3b94b066f3a4b7bcba67c0e4
Lamia123456 <112626217+Lamia123456@users.noreply.github.com>
2022-09-07 00:00:39 +0300

    lamia (log In & forget pass)

--- itmaen/lib/main.dart
+++ itmaen/lib/main.dart
@@ -3,45 +3,56 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 //Just For Testing that your database works ! Remove the code below after testing (Windows Usesrs)
-
+import 'login.dart';
 void main() async {
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp();
-runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 //test1
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
 // This widget is the root
 // of your application.
-@override
-Widget build(BuildContext context) {
-	return MaterialApp(
-	title: 'Firebase',
-	home: AddData(),
-	);
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase',
+      home: AddData(),
+    );
+  }
+   _MyAppState createState() => _MyAppState();
 }
 
 class AddData extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-	return Scaffold(
-	appBar: AppBar(
-		backgroundColor: Colors.green,
-		title: Text("geeksforgeeks"),
-	),
-	body:Center(
-		child: FloatingActionButton(
-		backgroundColor: Colors.green,
-		child: Icon(Icons.add),
-		onPressed: () {
-			FirebaseFirestore.instance
-				.collection('data')
-				.add({'text': 'FFF'});
-		},
-		),
-	),
-	);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("geeksforgeeks"),
+      ),
+      body: Center(
+        child: FloatingActionButton(
+          backgroundColor: Colors.green,
+          child: Icon(Icons.add),
+          onPressed: () {
+            FirebaseFirestore.instance.collection('data').add({'text': 'FFF'});
+          },
+        ),
+      ),
+    );
+  }
 }
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue[900],
+      ),
+      home: LoginPage(),
+    );
+  }
 }
