205ff370fcfc2ba72bcd61f1ab90d3900a2de1f3
aljoharash <aljoharash76@gmail.com>
2022-09-14 12:57:42 +0300

    Update adddialog.dart

--- itmaen/lib/pages/adddialog.dart
+++ itmaen/lib/pages/adddialog.dart
@@ -1,10 +1,10 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:itmaen/controller/addMedicineController.dart';
 import 'package:itmaen/model/medicinesModel.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:itmaen/secure-storage.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:itmaen/pages/addByScan.dart';
+import 'package:itmaen/pages/addManually.dart';
 
 class AddMedicine extends StatefulWidget {
   @override
@@ -12,100 +12,49 @@ class AddMedicine extends StatefulWidget {
 }
 
 class _AddMedicineState extends State<AddMedicine> {
-  String? genericName;
-  String? tradeName;
-  String? strengthValue;
-  String? unitOfStrength;
-  String? volume;
-  String? unitOfVolume;
-  String? packageSize;
-  String? barcode;
-  String? description;
-
-  final _firestore = FirebaseFirestore.instance;
-  final _auth = FirebaseAuth.instance;
-  String caregiverID = "";
-  late User loggedInUser;
-
-  @override
-  void initState() {
-    getCurrentUser();
-  }
-
-  void getCurrentUser() async {
-    try {
-      final user = await _auth.currentUser;
-      if (user != null) {
-        loggedInUser = user;
-        caregiverID = loggedInUser.uid;
-      }
-    } catch (e) {
-      print(e);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    Widget buildTextfield(
-        String hint, TextEditingController controller, var val) {
-      return Container(
-        margin: EdgeInsets.all(4),
-        child: TextField(
-          decoration: InputDecoration(
-              labelText: val,
-              border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                color: Colors.black38,
-              ))),
-          controller: controller,
-        ),
-      );
-    }
-
-    var mednameCont = TextEditingController();
-    var dosecont = TextEditingController();
-
-    //var medVal = addMedicineController().scannedMedicine[0].tradeName;
-    // var doseVal = addMedicineController().scannedMedicine[0].strengthValue;
+    // 
+    // var mednameCont = TextEditingController();
+    // var dosecont = TextEditingController();
 
     return Container(
-      padding: EdgeInsets.all(8),
-      height: 300,
+      padding: EdgeInsets.all(10),
+      height: 200,
       width: 400,
       child: Column(
         children: [
           Text(
-            'أضف دواء',
+            'إضافة دواء',
             style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 32,
                 color: Colors.blueGrey),
           ),
-          //  buildTextfield('اسم الدواء', mednameCont, medVal),
-          // buildTextfield('الجرعة', dosecont, doseVal),
+          //buildTextfield('اسم الدواء', mednameCont),
+          //buildTextfield('الجرعة', dosecont),
           ElevatedButton(
-            onPressed: () {
-              _firestore.collection('medicines').add({
-                'Generic name': genericName,
-                'Trade name': tradeName,
-                'Strength value': strengthValue,
-                'Unit of strength': unitOfStrength,
-                'Volume': volume,
-                'Unit of volume': unitOfVolume,
-                'Package size': packageSize,
-                'barcode': barcode,
-                'description': description,
-                'caregiverID': caregiverID,
-              });
-            },
-            child: Text('أضف الدواء'),
+            onPressed: () {Navigator.of(context).push(MaterialPageRoute(builder: (context)=>
+															addManually()));
+                              },
+            child: Text('أضف الدواء يدويًا'),
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(200,40),
+              maximumSize: const Size(200,40),
+            ),
           ),
           ElevatedButton(
-            onPressed: () => addMedicineController().scanBarcode(),
-            child: Text('الماسح الضوئي'),
+            onPressed: () {Navigator.of(context).push(MaterialPageRoute(builder: (context)=>
+															addByScan()));},
+            child:Icon(Icons.qr_code_scanner),
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(200,40),
+              maximumSize: const Size(200,40),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
