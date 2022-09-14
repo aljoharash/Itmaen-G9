19e520e0a44212afa8421d783b6d0ce29f6d0f2f
aljoharash <aljoharash76@gmail.com>
2022-09-14 23:22:25 +0300

    Update adddialog.dart

--- itmaen/lib/addMedicinePages/adddialog.dart
+++ itmaen/lib/addMedicinePages/adddialog.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:itmaen/controller/addMedicineController.dart';
 import 'package:itmaen/model/medicinesModel.dart';
 import 'package:itmaen/addMedicinePages/addByScan.dart';
@@ -20,16 +21,15 @@ class _AddMedicineState extends State<AddMedicine> {
 
     return Container(
       padding: EdgeInsets.all(10),
-      height: 200,
+      height: 160,
       width: 400,
       child: Column(
         children: [
           Text(
             'إضافة دواء',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 32,
-                color: Colors.blueGrey),
+            style: GoogleFonts.tajawal(fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 140, 167, 190),
+            )
           ),
           //buildTextfield('اسم الدواء', mednameCont),
           //buildTextfield('الجرعة', dosecont),
@@ -38,11 +38,12 @@ class _AddMedicineState extends State<AddMedicine> {
               Navigator.of(context)
                   .push(MaterialPageRoute(builder: (context) => addManually()));
             },
-            child: Text('أضف الدواء يدويًا'),
+            child: Text('أضف الدواء يدويًا'
+            , style:  GoogleFonts.tajawal(fontWeight: FontWeight.bold),),
             style: ElevatedButton.styleFrom(
               minimumSize: const Size(200, 40),
               maximumSize: const Size(200, 40),
-              //backgroundColor: Color.fromARGB(255, 140, 167, 190),
+              backgroundColor: Color.fromARGB(255, 140, 167, 190),
             ),
           ),
           ElevatedButton(
@@ -54,7 +55,7 @@ class _AddMedicineState extends State<AddMedicine> {
             style: ElevatedButton.styleFrom(
               minimumSize: const Size(200, 40),
               maximumSize: const Size(200, 40),
-              //  backgroundColor: Color.fromARGB(255, 140, 167, 190),
+              backgroundColor: Color.fromARGB(255, 140, 167, 190),
             ),
           ),
         ],
