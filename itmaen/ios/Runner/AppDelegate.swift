8fa5676618962bd832404df6d5f84e704cad0320
Sarah Alarifi <112616895+SarahAlarifi@users.noreply.github.com>
2022-09-22 11:15:38 +0300

    Notification Changes on AppDelegate

--- itmaen/ios/Runner/AppDelegate.swift
+++ itmaen/ios/Runner/AppDelegate.swift
@@ -5,15 +5,17 @@ import Flutter
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptions: {
+
+if #available(iOS 12.0, *) {
+  UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+}
+
+    [UIApplication.LaunchOptionsKey: Any]? }
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-
- if #available(iOS 10.0, *) {
-  UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-}
 
 
 }
