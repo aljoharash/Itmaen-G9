ab4c242083b29e01382ec7bd8db2a9dd4ff00674
Saly Ghorab <saly20012015@gmail.com>
2022-09-22 23:31:55 +0300

    Edits on notification to work on IOS

--- itmaen/lib/notification.dart
+++ itmaen/lib/notification.dart
@@ -1,20 +1,31 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:rxdart/subjects.dart';
 
-class Noti{
-  static Future initialize(FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
-    var androidInitialize = new AndroidInitializationSettings('mipmap/ic_launcher');
+class Noti {
+  static Future initialize(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var androidInitialize =
+        new AndroidInitializationSettings('mipmap/ic_launcher');
     var iOSInitialize = new IOSInitializationSettings();
-    var initializationsSettings = new InitializationSettings(android: androidInitialize,
-        iOS: iOSInitialize);
-    await flutterLocalNotificationsPlugin.initialize(initializationsSettings );
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+      requestSoundPermission: true,
+      requestBadgePermission: true,
+      requestAlertPermission: true,
+    );
+    var initializationsSettings = new InitializationSettings(
+        android: androidInitialize, iOS: initializationSettingsIOS);
+    await flutterLocalNotificationsPlugin.initialize(initializationsSettings);
   }
 
-  static Future showBigTextNotification({var id =0,required String title, required String body,
-    var payload, required FlutterLocalNotificationsPlugin fln
-  } ) async {
+  static Future showBigTextNotification(
+      {var id = 0,
+      required String title,
+      required String body,
+      var payload,
+      required FlutterLocalNotificationsPlugin fln}) async {
     AndroidNotificationDetails androidPlatformChannelSpecifics =
-    new AndroidNotificationDetails(
+        new AndroidNotificationDetails(
       'you_can_name_it_whatever1',
       'channel_name',
 
@@ -24,9 +35,12 @@ class Noti{
       priority: Priority.high,
     );
 
-    var not= NotificationDetails(android: androidPlatformChannelSpecifics,
-        iOS: IOSNotificationDetails()
-    );
-    await fln.show(0, title, body,not );
+    IOSNotificationDetails iosNotificationDetails = IOSNotificationDetails(
+        threadIdentifier: "thread1",
+        attachments: <IOSNotificationAttachment>[]);
+
+    var not = NotificationDetails(
+        android: androidPlatformChannelSpecifics, iOS: iosNotificationDetails);
+    await fln.show(0, title, body, not);
   }
 }
