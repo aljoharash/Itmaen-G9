ce25614e3c56b1aa5c1edc3762d7e671e0d5a173
aljoharash <aljoharash76@gmail.com>
2022-10-22 21:29:28 +0300

    animate tts icon

--- itmaen/lib/viewD.dart
+++ itmaen/lib/viewD.dart
@@ -2797,15 +2797,13 @@ class _medBubbleState extends State<medBubble> {
 
                                                 },
 
-                                                child: Icon(
+                                                child: 
 
                                                   TextToSpeechAPI().isPlaying !=
 
                                                           widget.doc
 
-                                                      ? Icons.volume_down
-
-                                                      : Icons.volume_up,
+                                                      ? Icon(Icons.volume_mute,
 
                                                   color: Color.fromARGB(
 
@@ -2813,7 +2811,24 @@ class _medBubbleState extends State<medBubble> {
 
                                                   size: 30,
 
-                                                ),
+                                                ) :
+                                                BlinkWidget(children: <Widget>[
+                                                  Icon(Icons.volume_mute,
+                                                  color: Color.fromARGB(
+                                                255, 111, 161, 200),
+                                                size: 30,),
+                                                Icon(Icons.volume_down,
+                                                  color: Color.fromARGB(
+                                                255, 111, 161, 200),
+                                                size: 30,),
+                                                Icon(Icons.volume_up,
+                                                  color: Color.fromARGB(
+                                                255, 111, 161, 200),
+                                                size: 30,),
+                                                ] )
+
+
+                                                
 
                                               )
 
@@ -3021,6 +3036,57 @@ class _medBubbleState extends State<medBubble> {
 
   }
 
+}
+
+
+class BlinkWidget extends StatefulWidget {
+  final List<Widget> children;
+  final int interval;
+
+  BlinkWidget({required this.children, this.interval = 500, Key? key}) : super(key: key);
+
+  @override
+  _BlinkWidgetState createState() => _BlinkWidgetState();
+}
+
+class _BlinkWidgetState extends State<BlinkWidget> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  int _currentWidget = 0;
+
+  initState() {
+    super.initState();
+
+    _controller = new AnimationController(
+      duration: Duration(milliseconds: widget.interval),
+      vsync: this
+    );
+
+    _controller.addStatusListener((status) {
+      if(status == AnimationStatus.completed) {
+        setState(() {
+          if(++_currentWidget == widget.children.length) {
+            _currentWidget = 0;
+          }
+        });
+
+        _controller.forward(from: 0.0);
+      }
+    });
+
+    _controller.forward();
+  }
+
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: widget.children[_currentWidget],
+    );
+  }
 }
 
  
